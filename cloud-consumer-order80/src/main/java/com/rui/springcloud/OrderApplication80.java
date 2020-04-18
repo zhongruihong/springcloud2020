6ae7cdbaaa66258320c7a19023828ee4b3877350
zrh <zhongruihong@foxmail.com>
2020-04-18 23:08:15 +0800

    单机版eureka，引入server和client

--- cloud-consumer-order80/src/main/java/com/rui/springcloud/OrderApplication80.java
+++ cloud-consumer-order80/src/main/java/com/rui/springcloud/OrderApplication80.java
@@ -2,7 +2,9 @@ package com.rui.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderApplication80 {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(OrderApplication80.class, args);
