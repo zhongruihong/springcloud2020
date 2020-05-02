4b2618e9f502e60518a437b3808ad2a8a44f6891
zrh <zhongruihong@foxmail.com>
2020-05-02 16:17:32 +0800

    搭建SpringCloud ConfigServer服务端：1、pom.xml引入spring-cloud-config-server；2、如果 引入数据库连接相关的包，如【jdbc】、【mysql】、【oracle】、【mybatis】、【druid】等，而application.yml没有配置数据源相关，会报找不到合适的驱动；3、启动类激活配置中心注解@EnableConfigServer；4、config.server.git.uri配置http地址；5、github上搭建相关配置文件的访问规则 ~

---
+++ cloud-config-center-3344/src/main/java/com/rui/springcloud/ConfigServerApplication3344.java
@@ -0,0 +1,22 @@
+package com.rui.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+/*
+ * Spring Framework Api：https://docs.spring.io/spring-framework/docs/current/javadoc-api/overview-summary.html
+ *  本项目启动后可以通过 访问http://configserver.rui.com:3344/master/config2020-dev.yml
+ *  前提是在windows的hosts文件中做了本地Ip的映射:127.0.0.1 configserver.rui.com
+ */
+@SpringBootApplication
+//@EnableEurekaClient //服务提供者 【eureka 客户端 】引入 spring-cloud-starter-netflix-eureka-client
+//@EnableDiscoveryClient//@EnableDiscoveryClient和@EnableEurekaClient共同点就是：都是能够让注册中心能够发现，扫描到该服务。不同点：@EnableEurekaClient只适用于Eureka作为注册中心，@EnableDiscoveryClient 可以是其他注册中心。
+@EnableConfigServer //激活配置中心
+public class ConfigServerApplication3344 {
+	/*mainboot自动提示生成，前提是pom.xml已经引入boot相关*/
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ConfigServerApplication3344.class, args);
+	}
+}
