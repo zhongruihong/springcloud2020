c79e64a311da3a0471ba4fdb9cddebdda088cc1e
zrh <zhongruihong@foxmail.com>
2020-05-03 13:10:54 +0800

    利用之间的8001和80，简单模拟了下zipkin链路监控使用~注意:1、下载启动zipkin的jar，默认端口9411；2、需要被链路监控的项目引入starter-zipkin；3、yml配置zipkin的url~

--- cloud-consumer-order80/src/main/java/com/rui/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/rui/springcloud/config/ApplicationContextConfig.java
@@ -22,6 +22,7 @@ public class ApplicationContextConfig {
 	//@LoadBalanced//单机版本只有一个服务，客户端url写死，不需要@LoadBalanced进行负载均衡；
 	//但集群版一个服务名称(spring.application.name)下有多个服务，客户端需要提供负载均衡能力@LoadBalanced，客户端才能通过服务名称调用（@LoadBalanced负载均衡默认方式是轮询）。
 	//如果没有该注解会报错，因为客户端只通过调用服务名称，并不知道自己要访问服务名称下的哪个服务（端口）
+	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
